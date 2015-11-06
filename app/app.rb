506da0f3b524b2104bc9ea002c8a645c58a3405a
Greg Buckner <greg@buckner.co>
2015-11-06 11:08:44 -0800

    initial

---
+++ app/app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'json'
+
+get '/' do
+  File.read('views/index.html')
+end
+
+get '/favorites' do
+  @movies = JSON.parse(File.read('data.json'))
+  erb :favorites
+end
+
+post '/favorites' do
+  return 'Invalid Request' unless params[:name] && params[:oid]
+
+  file = JSON.parse(File.read('data.json'))
+
+  new_movie = { name: params[:name], oid: params[:oid] }
+  file << movie
+  
+  File.write('data.json', JSON.pretty_generate(file))
+  
+  return movie.to_json
+end
